8b75f049aff2be2f769dda8b327acf902685835b
Danil Semelenov <mail@danil.mobi>
2014-12-06 22:27:31 +0300

    Separated syslinux install actions, add bash arg to syslinux-install_update

--- boot.sh
+++ boot.sh
@@ -3,15 +3,15 @@
 
 select-drive
 
-01-syslinux-install() {
-  chroot-bash <<EOF
-  pacman -S --noconfirm syslinux
-
-  syslinux-install_update -im
-EOF
+01-packages() {
+  chroot-cmd pacman -S --needed --noconfirm syslinux
 }
 
-02-syslinux-config() {
+02-syslinux-install() {
+  chroot-cmd bash `[[ ! $- =~ x ]] || echo -x` syslinux-install_update -im
+}
+
+03-syslinux-config() {
   sudo sed -i "s/^\(TIMEOUT\) .*/\1 10/" $mnt_dir/boot/syslinux/syslinux.cfg                                   
   root_uuid=`partition-uuid 2`
   sudo sed -i "s/^\(\s*APPEND root\)=[^ ]*/\1=UUID=$root_uuid/" $mnt_dir/boot/syslinux/syslinux.cfg
