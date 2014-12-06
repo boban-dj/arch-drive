cc5b95d861c30130e1f000262ccb3e30a6e82ec3
Danil Semelenov <mail@danil.mobi>
2014-12-06 21:27:17 +0300

    Add -q option to select-drive call in umount script

--- umount.sh
+++ umount.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 . ${BASH_SOURCE[0]%/*}/common.sh
 
-select-drive
+select-drive -q
 
-mount_paths=(`cat /etc/mtab | grep -oP "^$drive_path[p0-9](?= .+)" | tac || :`)
-[[ -z ${mount_paths:-} ]] || sudo umount ${mount_paths[@]}
+if [[ -n ${drive_path:-} ]]; then
+  mount_paths=(`cat /etc/mtab | grep -oP "^$drive_path[p0-9](?= .+)" | tac || :`)
+  [[ -z ${mount_paths:-} ]] || sudo umount ${mount_paths[@]}
+fi
 
 mount_paths=(`cat /etc/mtab | grep "^[^ ]* $mnt_dir[ /]" | cut -d ' ' -f 2 | tac || :`)
 [[ -z ${mount_paths:-} ]] || sudo umount ${mount_paths[@]}
