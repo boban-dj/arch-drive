93a8825e550a3009e95522a3d7a0c186e64b81ce
Danil Semelenov <mail@danil.mobi>
2014-12-07 04:10:05 +0300

    Add menu selection and drive changing

--- menu.sh
+++ menu.sh
@@ -1,4 +1,42 @@
 #!/bin/bash
 . "$(dirname `readlink -f "${BASH_SOURCE[0]}"`)"/common.sh
 
-echo TODO: menu
+select-drive
+detect-drive-name
+
+declare -A settings=(
+  [journaling]=on
+  [architecture]=$arch
+)
+
+while :; do
+  # TODO
+  settings_line=`printf ", %s" "${settings[@]}"`
+  settings_line=${settings_line:2}
+
+  menu_items=(
+    "Selected drive: $drive_name"
+    "Settings: $settings_line"
+    "Backup home partition"
+    "Format drive"
+    "Restore home partition"
+    "Setup system"
+    "Quit"
+  )
+
+  echo "Select action:"
+  select menu_item in "${menu_items[@]}"; do
+    [[ $menu_item == Quit ]] || echo
+
+    case $menu_item in
+      "Selected drive:"*)
+        select-drive -r
+        detect-drive-name
+        break
+        ;;
+      Quit)
+        exit
+        ;;
+    esac
+  done
+done
