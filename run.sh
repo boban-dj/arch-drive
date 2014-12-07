6a3975e63cef863e434a3db54ce79e810feeb8de
Danil Semelenov <mail@danil.mobi>
2014-12-07 23:03:16 +0300

    Add tool agnostic script downloading

--- run.sh
+++ run.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+download() {
+  if which curl >/dev/null; then
+    curl $1
+  elif which wget >/dev/null; then
+    wget -O - $1
+  else
+    echo "You need to have curl or wget installed." >&2
+    exit 1
+  fi
+}
+
 if [[ $0 == /dev/fd/* ]]; then
   mkdir -p /tmp/arch-drive/scripts
-  curl https://bitbucket.org/sgtpep/arch-drive/get/master.tar.gz | tar -xz -C /tmp/arch-drive/scripts --strip-components=1
+  download https://bitbucket.org/sgtpep/arch-drive/get/master.tar.gz | tar -xz -C /tmp/arch-drive/scripts --strip-components=1
 
   exec bash -$- /tmp/arch-drive/scripts/run.sh
 fi
