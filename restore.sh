6af36091f59661042d601accfd65053ba5906745
Danil Semelenov <mail@danil.mobi>
2014-12-08 01:03:15 +0300

    Add suppression of confirmation for restoring on empty partition

--- restore.sh
+++ restore.sh
@@ -10,7 +10,9 @@ if [[ ! -d ~/Downloads/arch-drive-home ]]; then
   exit
 fi
 
-read -p "Restore home partition content from backup? All existing home partition content will be replaced by content from backup! [y/N] "
-[[ $REPLY =~ ^[Yy] ]] || exit 0
+if [[ `find $mnt_dir/home -maxdepth 1 -! -name lost+found | sed 1d` ]]; then
+  read -p "Restore home partition content from backup? All existing home partition content will be replaced by content from backup! [y/N] "
+  [[ $REPLY =~ ^[Yy] ]] || exit 0
+fi
 
 sudo rsync -a --delete --exclude=/lost+found --info=progress2 ~/Downloads/arch-drive-home/ $mnt_dir/home
