f67f2b9e45fac71505bf0a642bd154611f2883b3
Danil Semelenov <mail@danil.mobi>
2014-12-07 03:27:51 +0300

    Fix run script

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-. "$(dirname `readlink -f "${BASH_SOURCE[0]}"`)"/common.sh
-
 if [[ $0 == /dev/fd/* ]]; then
   mkdir -p /tmp/arch-drive/scripts
-  curl https://bitbucket.org/sgtpep/arch-drive/get/master.tar.gz | tar -xz -C /tmp/arch-drive/scripts
+  curl https://bitbucket.org/sgtpep/arch-drive/get/master.tar.gz | tar -xz -C /tmp/arch-drive/scripts --strip-components=1
 
   exec bash -$- /tmp/arch-drive/scripts/run.sh
 fi
 
-run-script menu
+bash -$- "$(dirname `readlink -f "${BASH_SOURCE[0]}"`)"/menu.sh
