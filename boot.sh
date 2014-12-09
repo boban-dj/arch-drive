95cc52ec6af8b17fe62d6024fb1ffa158457643d
Danil Semelenov <mail@danil.mobi>
2014-12-10 00:37:40 +0300

    Add EFI bootloaders support

--- boot.sh
+++ boot.sh
@@ -17,4 +17,48 @@ do-configure-syslinux() {
   sudo sed -i "s/^\(\s*APPEND root\)=[^ ]*/\1=UUID=$root_uuid/" $mnt_dir/boot/syslinux/syslinux.cfg
 }
 
+do-copy-efi-applications() {
+  sudo mkdir -p $mnt_dir/boot/EFI/boot
+
+  sudo cp $mnt_dir/usr/lib/prebootloader/PreLoader.efi $mnt_dir/boot/EFI/boot/bootx64.efi
+  sudo cp $mnt_dir/usr/lib/prebootloader/HashTool.efi $mnt_dir/boot/EFI/boot/
+  sudo cp $mnt_dir/usr/lib/gummiboot/gummibootx64.efi $mnt_dir/boot/EFI/boot/loader.efi
+
+  sudo curl -o $mnt_dir/boot/EFI/boot/shellx64_v1.efi https://svn.code.sf.net/p/edk2/code/trunk/edk2/EdkShellBinPkg/FullShell/X64/Shell_Full.efi
+  sudo curl -o $mnt_dir/boot/EFI/boot/shellx64_v2.efi https://svn.code.sf.net/p/edk2/code/trunk/edk2/ShellBinPkg/UefiShell/X64/Shell.efi
+
+  sudo mkdir -p $mnt_dir/boot/EFI/Microsoft/Boot
+  sudo cp $mnt_dir/boot/EFI/boot/bootx64.efi $mnt_dir/boot/EFI/Microsoft/Boot/bootmgfw.efi
+}
+
+do-configure-gummiboot() {
+  sudo mkdir -p $mnt_dir/boot/loader
+
+  sudo tee $mnt_dir/boot/loader/loader.conf >/dev/null <<EOF
+timeout 1
+default arch
+EOF
+
+  sudo tee $mnt_dir/boot/loader/entries/arch.conf >/dev/null <<EOF
+title Arch Linux
+linux /vmlinuz-linux
+initrd /initramfs-linux.img
+options root=UUID=`partition-uuid 2` rw
+EOF
+  sudo tee $mnt_dir/boot/loader/entries/arch-fallback.conf >/dev/null <<EOF
+title Arch Linux Fallback
+linux /vmlinuz-linux
+initrd /initramfs-linux-fallback.img
+options root=UUID=`partition-uuid 2` rw
+EOF
+  sudo tee $mnt_dir/boot/loader/entries/uefi-shell-v1.conf >/dev/null <<EOF
+title UEFI Shell v1
+efi /EFI/boot/shellx64_v1.efi
+EOF
+  sudo tee $mnt_dir/boot/loader/entries/uefi-shell-v2.conf >/dev/null <<EOF
+title UEFI Shell v2
+efi /EFI/boot/shellx64_v2.efi
+EOF
+}
+
 run-actions
