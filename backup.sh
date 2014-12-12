8c603855582ea9e1ba34c6c50dc3c15a80b3917e
Danil Semelenov <mail@danil.mobi>
2014-12-12 04:46:42 +0300

    Add backup prevention on empty home partition

--- backup.sh
+++ backup.sh
@@ -5,5 +5,10 @@ select-drive
 
 run-script mount $drive_path
 
+if [[ ! `find $mnt_dir/home -maxdepth 1 -! -name lost+found | sed 1d` ]]; then
+  echo "Home partition is empty."
+  exit
+fi
+
 mkdir -p ~/Downloads/arch-drive-home
 sudo rsync -a --delete --exclude=/lost+found --info=progress2 $mnt_dir/home/ ~/Downloads/arch-drive-home
