145b2b2a0c7308c88bb12f233b25fe29a2b9d121
Danil Semelenov <mail@danil.mobi>
2015-03-11 12:07:41 +0300

    Add fix for external monitor output on some nvidia cards

--- boot.sh
+++ boot.sh
@@ -23,7 +23,7 @@ do-install-syslinux() {
 do-configure-syslinux() {
   sudo sed -i "s/^\(TIMEOUT\) .*/\1 10/" $mnt_dir/boot/syslinux/syslinux.cfg                                   
   root_uuid=`partition-uuid 2`
-  sudo sed -i "s/^\(\s*APPEND root\)=[^ ]*/\1=UUID=$root_uuid/" $mnt_dir/boot/syslinux/syslinux.cfg
+  sudo sed -i "s/^\(\s*APPEND root\)=[^ ]* rw/\1=UUID=$root_uuid rw nouveau.config=NvForcePost=1/" $mnt_dir/boot/syslinux/syslinux.cfg
   sudo sed -i "s|^\(\s*INITRD\) \(\.\./initramfs-linux\.img\)|\1 ../intel-ucode.img,\2|" $mnt_dir/boot/syslinux/syslinux.cfg
 }
 
@@ -60,13 +60,13 @@ title Arch Linux
 linux /vmlinuz-linux
 initrd /intel-ucode.img
 initrd /initramfs-linux.img
-options root=UUID=`partition-uuid 2` rw
+options root=UUID=`partition-uuid 2` rw nouveau.config=NvForcePost=1
 EOF
   sudo tee $mnt_dir/boot/loader/entries/arch-fallback.conf >/dev/null <<EOF
 title Arch Linux Fallback
 linux /vmlinuz-linux
 initrd /initramfs-linux-fallback.img
-options root=UUID=`partition-uuid 2` rw
+options root=UUID=`partition-uuid 2` rw nouveau.config=NvForcePost=1
 EOF
   sudo tee $mnt_dir/boot/loader/entries/uefi-shell-v1.conf >/dev/null <<EOF
 title UEFI Shell v1
